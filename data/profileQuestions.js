d36ebfe37c84bbe901be86e39b4ef1f627069078
Raymond Templo <rtemplo@gmail.com>
2017-10-20 17:35:00 -0400

    dynamicized listdog

---
+++ data/profileQuestions.js
@@ -0,0 +1,94 @@
+var profileQs = [
+    {
+        qText:"How often does your dog shed?", 
+        qAns:[
+            {ansText:"Way too much", ansVal:100},
+            {ansText:"A lot", ansVal:75},
+            {ansText:"Average", ansVal:50},
+            {ansText:"Very little", ansVal:25},
+            {ansText:"Barely at all", ansVal:0}
+        ]
+    },
+    {
+        qText:"How active is your dog?",
+        qAns:[
+            {ansText:"Incredibly Active", ansVal:100},
+            {ansText:"Very active", ansVal:75},
+            {ansText:"Average", ansVal:50},
+            {ansText:"Not very active", ansVal:25},
+            {ansText:"The calmest it gets", ansVal:0}
+        ]
+    },
+    {
+        qText:"How easy is/was it to train your dog?",
+        qAns:[
+            {ansText:"Very easy", ansVal:100},
+            {ansText:"Pretty easy", ansVal:75},
+            {ansText:"Average", ansVal:50},
+            {ansText:"Not so easy", ansVal:25},
+            {ansText:"Very hard", ansVal:0}
+        ]
+    },
+    {
+        qText:"How kid friendly is your dog?",
+        qAns:[
+            {ansText:"A kid's BFF for life", ansVal:100},
+            {ansText:"Very friendly", ansVal:75},
+            {ansText:"Pretty friendly", ansVal:50},
+            {ansText:"Not so friendly", ansVal:25},
+            {ansText:"Not for the young", ansVal:0}
+        ]
+    },
+    {
+        qText:"How easy is it to groom your dog?",
+        qAns:[
+            {ansText:"Incredibly easy", ansVal:100},
+            {ansText:"Very easy", ansVal:75},
+            {ansText:"Average", ansVal:50},
+            {ansText:"Not so easy", ansVal:25},
+            {ansText:"Very hard", ansVal:0}
+        ]
+    },        
+    {
+        qText:"What is the drool potential of your dog?",
+        qAns:[
+            {ansText:"A total drool factory", ansVal:100},
+            {ansText:"Pretty drooly", ansVal:75},
+            {ansText:"OK", ansVal:50},
+            {ansText:"Just a little", ansVal:25},
+            {ansText:"Nothing to speak of", ansVal:0}
+        ]
+    },        
+    {
+        qText:"How much of a barker is your dog?",
+        qAns:[
+            {ansText:"Way too much", ansVal:100},
+            {ansText:"A lot", ansVal:75},
+            {ansText:"Average", ansVal:50},
+            {ansText:"Just on the occasion", ansVal:25},
+            {ansText:"Pretty quiet", ansVal:0}
+        ]
+    },        
+    {
+        qText:"How well does your dog tolerate being alone?",
+        qAns:[
+            {ansText:"Completely independent", ansVal:100},
+            {ansText:"Very well", ansVal:75},
+            {ansText:"Well", ansVal:50},
+            {ansText:"Needs a good amount of attention", ansVal:25},
+            {ansText:"A constant companion", ansVal:0}
+        ]
+    },        
+    {
+        qText:"What is the approximate weight of your dog?",
+        qAns:[
+            {ansText:"90+ lbs", ansVal:100},
+            {ansText:"66-89 lbs", ansVal:75},
+            {ansText:"41-65 lbs", ansVal:50},
+            {ansText:"21-40 lbs", ansVal:25},
+            {ansText:"0-20 lbs", ansVal:0}
+        ]
+    }
+];
+
+module.exports = profileQs;
